ad86e2c858c2c4df633eac1f55076cdce9972424
josephaarond.bernardo <josephaarond.bernardo@gmail.com>
2025-07-29 12:27:26 +0800

    test6

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,7 +9,7 @@
       <div class="row justify-content-center">
         <div class="col-lg-8 m-4">
           <iframe
-            src="YOUR_Maps_EMBED_URL_HERE"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3862.6100523456885!2d121.0189080750711!3d14.492190385966468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c231766a5e1d%3A0x6a2c270d4c1d293d!2sSM%20City%20BF%20Paranaque!5e0!3m2!1sen!2sph!4v1701234567890!5m2!1sen!2sph"
             width="100%"
             height="400"
             style="border: 0"
